9174638b2e6b9944c40a9b257b1657aada67aa5e
sunlihuo <425705830@qq.com>
2018-01-04 12:06:04 +0800

    111

--- src/main/java/com/github/hls/service/RocketMQProducerServer.java
+++ src/main/java/com/github/hls/service/RocketMQProducerServer.java
@@ -4,21 +4,28 @@ import com.alibaba.rocketmq.client.producer.DefaultMQProducer;
 import com.alibaba.rocketmq.client.producer.SendResult;
 import com.alibaba.rocketmq.common.message.Message;
 import lombok.extern.log4j.Log4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
 
-@Service@Log4j
+@Service
+@Log4j
 public class RocketMQProducerServer {
 
     @Resource
     private DefaultMQProducer rocketMQProducer;
 
+    @Value("${rocketmq.producer.topic}")
+    private String topic;
+    @Value("${rocketmq.producer.tag}")
+    private String tag;
+
     public void sendJobName(String jobName) {
         try {
             Message message = new Message();
-            message.setTopic("simple_job");
-            message.setTags("notify");
+            message.setTopic(topic);
+            message.setTags(tag);
             message.setBody(jobName.getBytes("UTF-8"));
             SendResult sendResult = rocketMQProducer.send(message);
             log.info("发送MQ消息 sendJobName=" + sendResult);
